bf5699153866e2cc15ff95586cfab3e9203bbb20
Saroj Paudyal <Sarojpaudyal123@gmail.com>
2019-12-09 15:06:57 +0545

    test added

--- src/user/user.dao.ts
+++ src/user/user.dao.ts
@@ -3,3 +3,23 @@ import knex from '../utils/dbase';
 export function addUser(userObj: object): any {
     return knex("users").insert(userObj);
 }
+
+export function removeUser(userObj: object):any {
+    return knex('users').where(userObj).del();
+}
+
+export function updateUser<T extends {id: number}>(userObj: T):any {
+    return knex('users').where({
+        id : userObj.id
+    }).update(userObj)
+}
+
+export function getUser<T extends {id: number}>(userObj: T):any {
+    return knex('users').select('*').where({
+        id : userObj.id
+    });
+}
+
+export function getAllUsers(): any {
+    return knex('users').select('*');
+}
--- tests/user.dao.test.ts
+++ tests/user.dao.test.ts
@@ -1,9 +1,51 @@
-import { addUser } from '../src/user/user.dao';
+import {addUser, removeUser, getAllUsers, getUser, updateUser} from '../src/user/user.dao';
 
 test('add user', async () => {
     expect(await addUser({
         id : 2,
         name : 'saroj',
         age : 19
+    })).resolves;
+});
+
+test('remove user', async () => {
+    expect(await removeUser({
+        id : 2,
+        name : 'saroj',
+        age : 19
+    })).resolves;
+});
+
+test('get all users', async () => {
+    await addUser({
+        id : 3,
+        name : 'saroj',
+        age : 19
+    });
+    await addUser({
+        id : 4,
+        name : 'saroj',
+        age : 19
+    });
+    let users = await getAllUsers();
+    expect(users[0].id + users[1].id).toBe(7);
+});
+
+test('get user', async () => {
+    let user = await getUser({
+        id : 3
+    });
+    expect(user[0]).toEqual({
+        id : 3,
+        name : 'saroj',
+        age : 19
+    });
+});
+
+test('update user', async () => {
+    expect(await updateUser({
+        id : 2,
+        name : 'saroj',
+        age : 20
     })).resolves;
 });
